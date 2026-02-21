1b05182490263c1ea97ddc2a0385ba771591656f
Nhan Luong <tunhan281204@gmail.com>
2026-02-21 17:06:52 +0700

    feat: store note data by chrome storage api

--- tab-notes-extension/entrypoints/background.ts
+++ tab-notes-extension/entrypoints/background.ts
@@ -1,6 +1,5 @@
 export default defineBackground(() => {
   browser.tabs.onActivated.addListener(async (activeTab) => {
     const tab = await browser.tabs.get(activeTab.tabId);
-    console.log("Active tab: ", tab);
   });
 });
--- tab-notes-extension/entrypoints/popup/main.ts
+++ tab-notes-extension/entrypoints/popup/main.ts
@@ -1,5 +1,10 @@
 import "./styles.css";
 
+type NoteData = { note: string; createdAt: string; updatedAt: string };
+type NoteStorages = {
+  [key: string]: NoteData;
+};
+
 document.querySelector("#app")!.innerHTML = `
     <div class="sticky-note">
         <form class="note-form">
@@ -8,8 +13,9 @@ document.querySelector("#app")!.innerHTML = `
     </div>
 `;
 
-const noteText =
-  (document.getElementById("note-text") as HTMLTextAreaElement) || null;
+const noteText = document.getElementById(
+  "note-text",
+) as HTMLTextAreaElement | null;
 
 if (noteText) {
   noteText.addEventListener(
@@ -23,4 +29,32 @@ if (noteText) {
     },
     { once: true },
   );
+
+  const getPageKey = (url: string) => {
+    const urlObj = new URL(url);
+    return `${urlObj.hostname}${urlObj.pathname}`;
+  };
+
+  const storedNoteStorages = await browser.storage.sync.get("noteStorages");
+  const noteStorages: NoteStorages =
+    (storedNoteStorages.noteStorages as NoteStorages | undefined) ?? {};
+
+  noteText.addEventListener("keyup", async () => {
+    const [tab] = await browser.tabs.query({
+      active: true,
+      currentWindow: true,
+    });
+    const pageKey = getPageKey(tab.url as string);
+    const noteData: NoteData = {
+      note: noteText.value,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+    };
+
+    noteStorages[pageKey] = noteData;
+
+    browser.storage.sync.set({ noteStorages }, () => {
+      console.log("Value is set");
+    });
+  });
 }
